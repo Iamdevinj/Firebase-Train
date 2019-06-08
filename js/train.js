bf5863ac73bbb83a99cb6dff8df4ebfc433aacb4
Devin Jenkins <dvnjenks@gmail.com>
2019-06-08 06:05:08 -0700

    js finished, style sheets linked,some style added

--- js/train.js
+++ js/train.js
@@ -30,3 +30,17 @@ $("#addTrainBtn"),on("click",function(){
 
     return false;
 })
+trainData.ref().on("child_added",function(snapshot){
+    var name = snapshot.val().name;
+    var destination = snapshot.val().destination;
+    var frequency = snapshot.val().frequency;
+    var firstTrain = snapshot.val().firstTrain;
+
+    var remainder = moment().diff(moment.unix(firstTrain),"minutes")%frequency;
+    var minutes = frequency - remainder
+    var arrival = moment().add(minutes, "m").format("hh:mm A");
+
+$("#trainTable > tBody").append("<tr><td>"+name+"</td><td>"+destination+"</td><td>"+frequency+"</td><td>"+arrival+"</td><td>"+minutes+"</td><td>");
+    
+    
+})
